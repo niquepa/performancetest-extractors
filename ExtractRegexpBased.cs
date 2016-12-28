b8f0c0aa00e6aed7a42df3139fa914e25436ee29
Carlos Castillo <niquepa@gmail.com>
2016-12-28 16:43:25 -0500

    Update ExtractRegexpBased.cs

--- ExtractRegexpBased.cs
+++ ExtractRegexpBased.cs
@@ -7,24 +7,15 @@ using System.Text.RegularExpressions;
 
 namespace ExtractRegexpBased
 {
-    //-------------------------------------------------------------------------  
-    // This class creates a custom extraction rule named "Custom Extract Input"  
-    // The user of the rule specifies the name of an input field, and the  
-    // rule attempts to extract the value of that input field.  
-    //-------------------------------------------------------------------------  
+
     public class ExtractRegexpBased : ExtractionRule
     {
-        /// Specify a name for use in the user interface.  
-        /// The user sees this name in the Add Extraction dialog box.  
-        //---------------------------------------------------------------------  
+
         public override string RuleName
         {
             get { return "Extract string based on Regexp"; }
         }
-
-        /// Specify a description for use in the user interface.  
-        /// The user sees this description in the Add Extraction dialog box.  
-        //---------------------------------------------------------------------  
+        
         public override string RuleDescription
         {
             get { return "Extract string based on Regexp"; }
@@ -32,8 +23,6 @@ namespace ExtractRegexpBased
 
         public string regexp { get; set; }
         public string text { get; set; }
-        // The Extract method.  The parameter e contains the web performance test context.  
-        //---------------------------------------------------------------------  
 
         public override void Extract(object sender, ExtractionEventArgs e)
         {
@@ -41,7 +30,7 @@ namespace ExtractRegexpBased
             if (e.WebTest.Context[text] == null)
             {
                 e.Success = false;
-                throw new WebTestException(string.Format("No header location available"));
+                throw new WebTestException(string.Format("No text available"));
             }
             if (regexp == null || regexp == "")    {
                 e.Success = false;
@@ -49,7 +38,6 @@ namespace ExtractRegexpBased
             }
             Regex r = new Regex(regexp);
             Match m = r.Match(e.WebTest.Context[text].ToString());
-            //string vOrderId = m.Groups[0].ToString();
             e.WebTest.Context.Add(this.ContextParameterName, m.Groups[0].ToString());
             return;
         }
